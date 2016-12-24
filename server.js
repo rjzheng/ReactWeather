25e18c54175c2de76921e223df5acc4490f05c1a
rjzheng <rjzheng@ucsd.edu>
2016-12-24 12:45:40 -0800

    Heroku setup

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 
-// Create our app
+// Create express app
 var app = express();
+// Use default environment port or 8080 if run locally
+var PORT = process.env.PORT || 8080;
+
+app.use(function(req, res, next) {
+  // Check for http request
+  if(req.headers['x-forwarded-proto'] == 'http') {
+    next();
+  // Redirect to http if it's not
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(8080, function () {
-  console.log('Express server is up on port 8080');
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
